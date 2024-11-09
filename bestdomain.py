19f3167ffc00262acd355466aa3a13d4519865a4
jc-lw <baolw50@gmail.com>
2024-11-10 01:34:21 +0800

    Update bestdomain.py

--- bestdomain.py
+++ bestdomain.py
@@ -60,8 +60,8 @@ if __name__ == "__main__":
     
     # 示例URL和子域名对应的IP列表
     subdomain_ip_mapping = {
-        #'bestcf': 'https://ipdb.030101.xyz/api/bestcf.txt',
-        'api': 'https://raw.githubusercontent.com/jc-lw/youxuanyuming/refs/heads/main/ip.txt',
+        'bestcf': 'https://ipdb.030101.xyz/api/bestcf.txt',  # #域名一，bestcf.域名.com
+        'api': 'https://raw.githubusercontent.com/jc-lw/youxuanyuming/refs/heads/main/ip.txt', #域名二，api.域名.com
         # 添加更多子域名和对应的IP列表URL
     }
     
